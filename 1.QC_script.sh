5abcc6fb266180e3d5da147dc98d2bd65a1758cf
Yuwei_Yang <95574349+yangyuwei75@users.noreply.github.com>
2026-03-28 16:31:43 +0100

    Enhance QC script for parallel execution
    Updated job script to include parallel processing and changed input file paths.

--- 1.QC_script.sh
+++ 1.QC_script.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+#SBATCH --cpus-per-task=8
 #SBATCH --job-name=fastqc
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=07:00:00
 
 # set up job environment
 module load Java/11.0.27
 module load FastQC/0.12.1-Java-11
-# working folder: /scratch/brussel/108/vsc10841/RHome
+module load parallel/20250722-GCCcore-14.2.0
+# working folder: /scratch/brussel/vo/000/bvo00026/vsc10841/Mapping_RNAseq_heps_mono
+cd /scratch/brussel/vo/000/bvo00026/vsc10841/Mapping_RNAseq_heps_mono
+mkdir ./QC_RAW/
+max_jobs=16
 
-for i in ./Data_monocyte_infiltration/*.fastq.gz
+for i in /scratch/brussel/vo/000/bvo00026/vsc10841/RNAseq_heps_20260327/*.fastq.gz
 do
- fastqc -o ./QC_RAW/ $i
+ fastqc -o ./QC_RAW/ "$i" & 
+ if [[ $(jobs -r -p | wc -l) -ge $max_jobs ]]; then
+    wait -n
+ fi
 done
